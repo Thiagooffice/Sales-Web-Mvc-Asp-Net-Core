9c68e45347b9c745e40e1e752ef5cb7c6cc914b4
Thiago <thiagooffice2@gmail.com>
2024-03-13 15:08:23 -0300

    feat: first controller and razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App";
+            ViewData["Email"] = "thiagooffice2@gmail.com";
 
             return View();
         }
