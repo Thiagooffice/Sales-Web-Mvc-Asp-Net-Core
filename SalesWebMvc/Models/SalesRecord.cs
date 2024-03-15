65350d5975840916409ac747f8958d413f4c14fc
Thiago <thiagooffice2@gmail.com>
2024-03-15 10:33:15 -0300

    feat: Other entities and second migration

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -8,6 +8,20 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
-        public SalesStatus Status { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
     }
 }
